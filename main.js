a8d112e7a5d23b8d1b13fc412fcb344c47e71938
Zaneta Szalanska <zaneta.szalanska@gmail.com>
2020-03-27 17:07:39 +0100

    update index.html, style.css, main.js, add photos

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const iconBurger = document.querySelector('.fa-bars');
 const nav = document.querySelector('nav.burger');
+const navHeight = nav.scrollHeight;
+const divMove = document.querySelector('div.move');
 
 
 menuBurger = function () {
 	console.log('ok');
 	nav.classList.toggle('active');
+	if (nav.classList.contains('active')) {
+		divMove.style.top = navHeight + 'px';
+	} else {
+		divMove.style.top = 0;
+	}
 }
 
 
